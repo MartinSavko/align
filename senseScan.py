4b1b4f6cc7e76e53bc43b67fe6424af848d6cae5
Martin Savko <martin.savko@gmail.com>
2013-07-31 18:18:28 +0200

    Sense scan improvements

--- senseScan.py
+++ senseScan.py
@@ -7,7 +7,7 @@ import numpy
 import gauss2d
 import scipy.ndimage
 
-def loadResults(fileName='aperture_scan_Thu_Jul_11_03:33:22_2013.pck'):
+def loadResults(fileName):
     f = open(fileName)
     results = pickle.load(f)
     f.close()
@@ -63,19 +63,30 @@ def plot_surface_wire(X, Y, Z, filename='resultFigure.png', stride=1):
     from matplotlib import cm
     import matplotlib.pyplot as plt
 
-    fig = plt.figure(figsize=plt.figaspect(0.5))
+    fig = plt.figure(filename.replace('.png', ''), figsize=plt.figaspect(0.5))
+    # surface
     
-    # surf
-    ax = fig.add_subplot(1, 2, 1, projection='3d')
-    surf = ax.plot_surface(X, Y, Z, rstride=stride, cstride=stride, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-    fig.colorbar(surf, shrink=0.5, aspect=10)
+    ax = fig.add_subplot(1, 3, 1, projection='3d', title='Grey')
+    surf = ax.plot_surface(X, Y, Z, rstride=stride, cstride=stride, cmap=cm.Greys, linewidth=0, antialiased=True)
+    ax.view_init(elev=8., azim=-49.)
+    fig.colorbar(surf, shrink=0.5, aspect=15)
+    
+    ax = fig.add_subplot(1, 3, 2, projection='3d', title='Bone')
+    surf = ax.plot_surface(X, Y, Z, rstride=stride, cstride=stride, cmap=cm.bone, linewidth=0, antialiased=True)
+    ax.view_init(elev=8., azim=-49.)
+    fig.colorbar(surf, shrink=0.5, aspect=15)
     
     # wire
-    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax = fig.add_subplot(1, 3, 3, projection='3d', title='Wireframe')
     wire = ax.plot_wireframe(X, Y, Z, rstride=stride, cstride=stride)
+    ax.view_init(elev=8., azim=-49.)
+    ## mesh
+    #ax = fig.add_subplot(1, 4, 3, projection='3d', title='Wireframe')
+    #wire = ax.mesh(X, Y, Z, rstride=stride, cstride=stride)
+
+    # save and display
     plt.savefig(filename)
     plt.show()
-    
     
 def XYZ(xyz, shape=(20, 40), observable = ('self.imag', 'image'), what = 'capillary'):
     '''Go through the results and return X, Y, Z matrices for 3d plots'''
@@ -115,7 +126,7 @@ def main():
     usage = 'Program to analyze results of grid scan done on apertures and capillary beamstop of MD2. The only input is the filename of the file storing pickled dictionary of results. By default the program will try to find the center of the scanned object using maximum, gaussian fit and center of mass.'
     parser = optparse.OptionParser(usage = usage)
 
-    parser.add_option('-f', '--filename', type = str, help = 'File with the scan results')
+    parser.add_option('-f', '--filename', default='aperture_100um_Tue_Jul_23_18:42:40_2013.pck', type = str, help = 'File with the scan results, (default: %default)')
     
     (options, args) = parser.parse_args()
     print options
@@ -138,7 +149,7 @@ def main():
     print 'Y[i,j]', Y[i][j]
     print 'Z[i,j]', Z[i][j]
     
-    Z = (Z > 0.8*m) * Z
+    #Z = (Z > 0.8*m) * Z
     params = fitGauss(Z)
     print 'Gauss fit parameters', params
     ig = int(round(params[1]))
